43c65881c9edbbaa60992ec648aad6fcbe8092f2
Isaac Aderogba <isaacaderogba1@gmail.com>
2020-06-29 05:57:28 +0100

    Sliding window pattern

--- src/patterns/guided.ts
+++ src/patterns/guided.ts
@@ -152,3 +152,38 @@ export function sumZeroGuided(arr: number[]) {
     }
   }
 }
+
+/**
+ * Sliding Window Pattern
+ * ---
+ * Create Window which could be single variable or which could be a sub array.
+ * Depending on the condition, the window either increases or closes
+ *
+ * Accept an array of integers and a number n, calculate the max sum of n
+ * consecutive elements in the array
+ */
+
+export function maxSubarraySum(arr: number[], num: number) {
+  if (!arr.length) return null;
+
+  let maxSum = 0;
+  let tempSum = 0;
+
+  for (let i = 0; i < num; i++) {
+    maxSum += arr[i];
+  }
+
+  tempSum = maxSum;
+
+  for (let i = num; i < arr.length; i++) {
+    tempSum = tempSum - arr[i - num] + arr[i];
+    maxSum = Math.max(maxSum, tempSum);
+  }
+
+  console.log(maxSum);
+  return maxSum;
+}
+
+maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 2); // 10
+maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 4); // 10
+maxSubarraySum([], 4); // null
