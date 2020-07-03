2371185d5db0b4668cfc40c1b6e083aae5bbf190
Isaac Aderogba <isaacaderogba1@gmail.com>
2020-07-03 05:38:39 +0100

    complete recusrive productOfArray

--- src/recursion/challenge.ts
+++ src/recursion/challenge.ts
@@ -22,3 +22,16 @@ export function factorial(num: number): number {
 }
 
 // console.log(factorial(7)); // 5040
+
+export function productOfArray(arr: number[]): number {
+  let count = arr.length - 1;
+
+  function helper(count: number): number {
+    if (count < 0) return 1;
+    return arr[count] * helper(count - 1);
+  }
+
+  return helper(count);
+}
+
+console.log(productOfArray([1, 2, 3, 10])); // 6
