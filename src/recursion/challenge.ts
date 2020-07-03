f3ce49385a2596f2325794ea77448865ec2b111a
Isaac Aderogba <isaacaderogba1@gmail.com>
2020-07-03 05:28:27 +0100

    create recursive power function

--- src/recursion/challenge.ts
+++ src/recursion/challenge.ts
@@ -1,1 +1,18 @@
-export {};
+/**
+ * Write a function called power which accepts a base and an exponent.
+ * The function should return the power of the base to the exponent.
+ */
+
+export function power(base: number, exponent: number) {
+  let count = exponent;
+
+  function helper(count: number): number {
+    if (count === 1) return base;
+    return base * helper(count - 1);
+  }
+
+  return helper(count);
+}
+
+// console.log(power(2, 2)); // 4
+// console.log(power(2, 4)); // 16
