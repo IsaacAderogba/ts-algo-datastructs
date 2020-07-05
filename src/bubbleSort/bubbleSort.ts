f1cf8fae4836f3e58d63ff244c99e8ff0f4ae36c
Isaac Aderogba <isaacaderogba1@gmail.com>
2020-07-05 16:31:54 +0100

    Add no swaps

--- src/bubbleSort/bubbleSort.ts
+++ src/bubbleSort/bubbleSort.ts
@@ -13,13 +13,19 @@ export function bubbleSort(arr: number[], direction: "asc" | "desc" = "asc") {
 }
 
 console.log(bubbleSort([5, 3, 4, 1, 2], "desc"));
-
+/**
+ * For the last time through, did we make any swaps?
+ * If the answer is no, then we're actually done.
+ */
 export function guidedBubbleSort(arr: number[]) {
+  let noSwaps = true;
   for (let i = arr.length; i > 0; i--) {
     for (let j = 0; j < i - 1; j++) {
       let temp = arr[j];
       arr[j] = arr[j + 1];
       arr[j + 1] = temp;
+      noSwaps = false;
     }
+    if (noSwaps) break;
   }
 }
