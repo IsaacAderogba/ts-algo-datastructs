583d58cde3dd6ee6995dce0e1884c48ff588f005
Isaac Aderogba <isaacaderogba1@gmail.com>
2020-07-07 06:27:44 +0100

    Naive implementation

--- src/sorting/mergeSort.ts
+++ src/sorting/mergeSort.ts
@@ -8,22 +8,87 @@
  * We need to write a function to merge two sorted arrays
  */
 
-function merge(arr1: number[], arr2: number[]) {
+/**
+  * 
+  * @param arr1 function merge(arr1: number[], arr2: number[]) {
   let arrayToReturn: number[] = [];
   let leftArrPtr = 0;
   let rightArrPtr = 0;
 
   while (leftArrPtr < arr1.length || rightArrPtr < arr2.length) {
-    if (arr1[leftArrPtr] <= arr2[rightArrPtr]) {
+    if (arr1[leftArrPtr] < arr2[rightArrPtr]) {
       arrayToReturn.push(arr1[leftArrPtr]);
       leftArrPtr++;
-    } else {
+    } else if (arr2[rightArrPtr] < arr1[leftArrPtr]) {
       arrayToReturn.push(arr2[rightArrPtr]);
       rightArrPtr++;
+    } else {
+      break;
     }
   }
 
   return arrayToReturn;
 }
+  */
 
-console.log(merge([1, 10, 50], [2, 14, 99, 100]));
+function merge(arr1: number[], arr2: number[]) {
+  let results = [];
+  let i = 0;
+  let j = 0;
+  while (i < arr1.length && j < arr2.length) {
+    if (arr2[j] > arr1[i]) {
+      results.push(arr1[i]);
+      i++;
+    } else {
+      results.push(arr2[j]);
+      j++;
+    }
+  }
+  while (i < arr1.length) {
+    results.push(arr1[i]);
+    i++;
+  }
+  while (j < arr2.length) {
+    results.push(arr2[j]);
+    j++;
+  }
+  return results;
+}
+
+/**
+ * slice up the array in halves, call mergeSort again and break each half again
+ * Base case is when length is 1 or less than
+ */
+
+function mergeSort(arr: number[]) {
+  let arrays: number[][] = [];
+
+  function mergeHelper(arr: number[]) {
+    if (arr.length <= 1) {
+      arrays.push(arr);
+      return;
+    }
+
+    const pivotPoint = Math.floor(arr.length / 2);
+    const [slice1, slice2] = [
+      arr.slice(0, pivotPoint),
+      arr.slice(pivotPoint),
+      arr.length - 1,
+    ];
+
+    mergeHelper(slice1);
+    mergeHelper(slice2);
+  }
+
+  mergeHelper(arr);
+
+  while (arrays.length > 1) {
+    const arr1 = arrays.pop();
+    const arr2 = arrays.pop();
+    const mergedArray = merge(arr1 || [], arr2 || []);
+    arrays.push(mergedArray);
+  }
+  return arrays.pop();
+}
+
+console.log(mergeSort([5, 1, 29, 2, 45, 4, 13]));
