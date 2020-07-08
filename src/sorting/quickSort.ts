6a7410d38154725a7478d822170ccbb1c71d43c7
Isaac Aderogba <isaacaderogba1@gmail.com>
2020-07-08 06:00:49 +0100

    Quick sort implementation

--- src/sorting/quickSort.ts
+++ src/sorting/quickSort.ts
@@ -34,3 +34,21 @@ export function pivot(arr: number[], start = 0, end = arr.length + 1) {
 }
 
 pivot([4, 8, 2, 1, 5, 7, 6, 3]);
+
+/**
+ * Takes first item, returns index - call quick sort on left and right side of pivot point
+ * call up until but not including the pivot
+ * recursively call on subarray to the left of the index, and subarrays to the right
+ * base case is checking the sub array
+ */
+
+export function quickSort(arr: number[], left = 0, right = arr.length - 1) {
+  if (left < right) {
+    let pivotIndex = pivot(arr, left, right);
+
+    // don't include the pivot
+    quickSort(arr, left, pivotIndex - 1);
+    quickSort(arr, pivotIndex + 1, right);
+  }
+  return arr;
+}
