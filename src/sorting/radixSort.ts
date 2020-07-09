47c9cd7d0f773b25a06ebc7156ab82c6073d6b18
Isaac Aderogba <isaacaderogba1@gmail.com>
2020-07-09 05:35:31 +0100

    Create most digits helpers

--- src/sorting/radixSort.ts
+++ src/sorting/radixSort.ts
@@ -14,3 +14,11 @@ function digitCount(num: number) {
   if (num === 0) return 1;
   return Math.floor(Math.log10(Math.abs(num))) + 1;
 }
+
+function mostDigits(nums: number[]) {
+  let maxDigits = 0;
+  for (let i = 0; i < nums.length; i++) {
+    maxDigits = Math.max(maxDigits, digitCount(nums[i]));
+  }
+  return maxDigits;
+}
