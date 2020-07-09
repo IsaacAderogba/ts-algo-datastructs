9f3963e3396eddd339de2074d78a262ba41fe0de
Isaac Aderogba <isaacaderogba1@gmail.com>
2020-07-09 05:30:24 +0100

    Create getDigit radix helper

---
+++ src/sorting/radixSort.ts
@@ -0,0 +1,11 @@
+/**
+ * Group of algorithms that don't make direct comparisons.
+ * So how the heck does it work. Works with numbers
+ * - Info about size is encoded in its number of digits
+ * - Do 1 digit numbers first (look at first digit in right side)
+ */
+
+ // divide by 100s place, divide, and then mod 10
+function getDigit(num: number, i: number) {
+  return Math.floor(Math.abs(num) / Math.pow(10, i)) % 10;
+}
