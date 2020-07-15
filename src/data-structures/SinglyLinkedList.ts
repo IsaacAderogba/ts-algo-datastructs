fae907391a3909a9cc0027deee8865d8267835ba
Isaac Aderogba <isaacaderogba1@gmail.com>
2020-07-15 06:34:21 +0100

    Naive pop implementation

--- src/data-structures/SinglyLinkedList.ts
+++ src/data-structures/SinglyLinkedList.ts
@@ -42,7 +42,37 @@ class SinglyLinkedList<T> {
     this.length++;
     return this;
   }
+
+  pop() {
+    if (!this.tail || !this.head) return;
+    if (this.head === this.tail) {
+      const temp = this.head;
+      this.head = null;
+      this.tail = null;
+      this.length--;
+      return temp;
+    }
+
+    let pre = this.head;
+    let isLast = this.head;
+
+    while (pre.next) {
+      isLast = pre;
+      pre = pre.next;
+    }
+
+    const nodeToReturn = isLast.next;
+    isLast.next = null;
+    this.tail = isLast;
+
+    this.length--;
+    return nodeToReturn;
+  }
 }
 
 const list = new SinglyLinkedList<string>();
 list.push("hi");
+list.push("there");
+console.log(list.pop());
+console.log(list.pop());
+console.log(list);
