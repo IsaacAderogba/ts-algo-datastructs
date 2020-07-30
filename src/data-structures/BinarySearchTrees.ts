7e590c14d59fbdfb1f9c26c274c2c6d93826a476
Isaac Aderogba <isaacaderogba1@gmail.com>
2020-07-30 06:00:23 +0100

    Blurb on trees

---
+++ src/data-structures/BinarySearchTrees.ts
@@ -0,0 +1,10 @@
+/**
+ * Bit of a step up from Linked Lists - different types of trees (trees, binary trees, binary search trees)
+ * Tree: A data structure that consists of nodes in a parent/child relationship
+ * Can't have siblings point to siblings, strictly parent pointing to child
+ * Siblings are nodes with the same parent
+ * Leaf is a node with no children
+ * Edge is the connection between one node and another
+ * 
+ * Lists are linear, Trees are nonlinear (multiple ways to traverse a tree)
+ */
