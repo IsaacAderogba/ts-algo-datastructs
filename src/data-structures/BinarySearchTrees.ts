f435a6446a459e53ffdf580f34533534b8ee6215
Isaac Aderogba <isaacaderogba1@gmail.com>
2020-07-31 05:25:49 +0100

    Tree examples

--- src/data-structures/BinarySearchTrees.ts
+++ src/data-structures/BinarySearchTrees.ts
@@ -7,4 +7,12 @@
  * Edge is the connection between one node and another
  * 
  * Lists are linear, Trees are nonlinear (multiple ways to traverse a tree)
+ * 
+ * Examples
+ * - HTML DOM (dev tools and debugger)
+ * - Network routing
+ * - Abstract Syntax Tree (parsing and making sure it's valid syntax)
+ * - Artificial Intelligence (Mini-Max decision tree for Tic Tac Toe game, look at all combinations)
+ * - Folders in an operating system (Things with parent-child relationships are often trees)
+ * - JSON
  */
