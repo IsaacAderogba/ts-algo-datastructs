24147ec234baef7b11c2d41afebf142472eba887
Isaac Aderogba <isaacaderogba1@gmail.com>
2020-07-31 05:35:56 +0100

    Setup BST and Node

--- src/data-structures/BinarySearchTrees.ts
+++ src/data-structures/BinarySearchTrees.ts
@@ -5,9 +5,9 @@
  * Siblings are nodes with the same parent
  * Leaf is a node with no children
  * Edge is the connection between one node and another
- * 
+ *
  * Lists are linear, Trees are nonlinear (multiple ways to traverse a tree)
- * 
+ *
  * Examples
  * - HTML DOM (dev tools and debugger)
  * - Network routing
@@ -15,4 +15,25 @@
  * - Artificial Intelligence (Mini-Max decision tree for Tic Tac Toe game, look at all combinations)
  * - Folders in an operating system (Things with parent-child relationships are often trees)
  * - JSON
+ * - Figma
+ *
+ * Binary Trees (Also Ternary trees)
+ * - Each node can have AT MOST 2 children
+ * - BSTs are special cases, that are sorted in a particular way
+ *    - Used for when there's a way to compare things (some bigger, some less)
+ *    - Search and Insertion become very quick (allows divide and conquer)
  */
+
+export class Node<T> {
+  value: T;
+  left: Node<T> | null = null;
+  right: Node<T> | null = null;
+
+  constructor(value: T) {
+    this.value = value;
+  }
+}
+
+export class BinarySearchTree<T> {
+  root: Node<T> | null = null;
+}
